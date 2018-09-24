f589f99c8b176792944d5391b66b2925ffa762e7
Jan van der velden <jan114852@outlook.com>
2018-09-24 12:26:04 +0200

    J1W4/5B1

--- game.js
+++ game.js
@@ -1,1 +1,40 @@
-alert ("kies de juiste antwoorden en score zo veel mogelijk punten!                           voor elke goede vraag krijgt u 1 punt.")
+ alert ("kies de juiste antwoorden en score zo veel mogelijk punten! voor elk goed antwoord krijgt u 1 punt. 1-3 is noob, 4-7 is goed, 8-10 is pro.");
+
+ var een = prompt ("hoeveel mensen staan er in de basis van een voetbalteam? 1) 10, 2) 11, 3) 22");
+      if (een == "1"){
+       document.write("FOUT! het goede antwoord is antwoord 2, je krijgt geen punten".)
+      }
+           else if (een == "2"){
+             document.write("GOED! je krijgt 1 punt.");
+           }
+                else if (een == "3"){
+                  document.write("FOUT! het goede antwoord is antwoord 2, je krijgt geen punten.");
+                }
+                     document.write("<br>");
+ 
+
+ var twee = prompt ("Mag de doelman de bal pakken als zijn teamgenoot de bal naar hem speelt? 1) ja dat mag. 2) nee, de tegenstander krijgt een penalty. 3) nee, de tegenstander krijgt een indirecte vrije trap.");
+      if (twee == "1"){
+       document.write("FOUT! het goede antwoord is antwoord 3, je krijgt geen punten.");
+      }
+           else if (twee == "2"){
+             document.write("FOUT! het goede antwoord is antwoord 3, je krijgt geen punten.");
+           }
+                else if (twee == "3"){
+                  document.write("GOED! je krijgt 1 punt.");
+                }
+                     document.write("<br>");
+ 
+
+ var drie = prompt ("Wanneer heb je een ploeg een jantje gegeven? 1) bij 5-0. 2) bij 3-0 of 3) bij 10-0.");
+       if (drie == "1"){
+       document.write("FOUT! het goede antwoord is antwoord 2, je krijgt geen punten.");
+      }
+           else if (drie == "2"){
+             document.write("GOED! je krijgt 1 punt.");
+           }
+                else if (drie == "3"){
+                  document.write("FOUT! het goede antwoord is antwoord 2, je krijgt geen punten.");
+                }
+
+
